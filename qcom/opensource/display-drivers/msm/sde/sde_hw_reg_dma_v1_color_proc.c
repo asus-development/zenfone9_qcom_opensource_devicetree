5c9095a28c6adf3328b60bb0c10aa49e6898a745
micky387 <mickaelsaibi@free.fr>
2025-11-22 10:58:04 -0500

    display-drivers: Drop the custom PXLW iris lut in v1 color proc
    We dont use pixelworks. We build Display hal from source
    
    Signed-off-by: micky387 <mickaelsaibi@free.fr>

--- qcom/opensource/display-drivers/msm/sde/sde_hw_reg_dma_v1_color_proc.c
+++ qcom/opensource/display-drivers/msm/sde/sde_hw_reg_dma_v1_color_proc.c
@@ -4130,24 +4130,10 @@ void reg_dmav2_setup_dspp_igcv4(struct sde_hw_dspp *ctx, void *cfg)
 		data[j++] = (u16)(lut_cfg->c0[i] << 4);
 		data[j++] = (u16)(lut_cfg->c1[i] << 4);
 	}
-	// set last IGC values
-#if 1 //defined(CONFIG_PXLW_IRIS)
-	data[j++] = (u16)(lut_cfg->c2_last << 4);
-	data[j++] = (u16)(lut_cfg->c0_last << 4);
-	data[j++] = (u16)(lut_cfg->c1_last << 4);
-#else
 	data[j++] = lut_cfg->c2_last ? (u16)(lut_cfg->c2_last << 4) : (u16)(4095 << 4);
 	data[j++] = lut_cfg->c0_last ? (u16)(lut_cfg->c0_last << 4) : (u16)(4095 << 4);
 	data[j++] = lut_cfg->c1_last ? (u16)(lut_cfg->c1_last << 4) : (u16)(4095 << 4);
-#endif
 
-// ASUS BSP Display +++
-#if defined ASUS_AI2202_PROJECT
-	if (ai2202_need_skip_data((u32)lut_cfg->c2_last)) {
-		DSI_LOG("skip c2=%d\n", lut_cfg->c2_last);
-		goto exit;
-	}
-#endif
 	REG_DMA_SETUP_OPS(dma_write_cfg, 0, (u32 *)data, len,
 			REG_BLK_LUT_WRITE, 0, 0, 0);
 	/* table select is only relevant to SSPP Gamut */
